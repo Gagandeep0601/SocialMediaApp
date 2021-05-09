f6024d8552673604675141fb693ecbfe0f665f21
Gagandeep0601 <gagandeepkaur01@yahoo.com>
2021-05-09 15:03:23 +0530

    Google SignIn feature

--- app/src/main/java/com/bignerdranch/socialmediaapp/LoginActivity.java
+++ app/src/main/java/com/bignerdranch/socialmediaapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.socialmediaapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,20 +19,31 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 100 ;
     EditText mEmail, mPassword;
     TextView mNotHaveAccount , mForgetPassword;
     Button mLogin;
     private FirebaseAuth mAuth;
     ProgressDialog mPd;
+    SignInButton mGoogleLoginBtn ;
+    GoogleSignInClient mGoogleSignInClient;
 
 
     @Override
@@ -43,11 +55,21 @@ public class LoginActivity extends AppCompatActivity {
         mNotHaveAccount = findViewById(R.id.notAlreadyAccount);
         mLogin = findViewById(R.id.login_button);
         mForgetPassword = findViewById(R.id.ForgetPassword);
+        mGoogleLoginBtn = findViewById(R.id.googleLoginBtn);
         mPd = new ProgressDialog(LoginActivity.this);
+
+        // Configure Google Sign In (before mAuth)
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         //init firebase auth
         mAuth = FirebaseAuth.getInstance();
         actionBar();
         clickLoginButton();
+        googleLoginBtn();
         //not have a account, jump to register Activity
         mNotHaveAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +84,62 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showForgetPasswordDialog();  // jump to a function to recover a forget password
+            }
+        });
+    }
+
+    // handle Listener for google login button
+    private void googleLoginBtn() {
+        mGoogleLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Google Login starts
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "signInWithCredential:success "+user.getEmail(), Toast.LENGTH_SHORT).show();
+                            // login , go to ProfileActivity
+                            startActivity(new Intent(LoginActivity.this,ProfileActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Login Failed...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -163,6 +241,7 @@ public class LoginActivity extends AppCompatActivity {
                             FirebaseUser user = mAuth.getCurrentUser();
                             // login , go to ProfileActivity
                             startActivity(new Intent(LoginActivity.this,ProfileActivity.class));
+                            finish();
                         } else {
                             // If sign in fails, display a message to the user. and dismiss progress dialog
                             mPd.dismiss();
